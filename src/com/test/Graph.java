2eb43f8c81eebacce27fe1bc35bbe9f3f9773efa
James Bugara <james.bugara@mastercard.com>
2022-03-13 22:20:52 +0300

    Graph topological sort

--- src/com/test/Graph.java
+++ src/com/test/Graph.java
@@ -3,21 +3,31 @@ package com.test;
 import java.util.*;
 
 public class Graph {
-    public static void main(String[] args ){
-        Graph graph= new Graph();
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+//        graph.addNode("A");
+//        graph.addNode("B");
+//        graph.addNode("C");
+//        graph.addNode("D");
+//        graph.addNode("E");
+//        graph.addEdge("A", "B");
+//        graph.addEdge("A", "C");
+//        graph.addEdge("A", "D");
+//        graph.addEdge("C", "D");
+//        graph.addEdge("A", "E");
+//        // graph.traverseDepthFirst("A");
+//        graph.traverseBreadthFirst("A");
+        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        graph.addNode("E");
-        graph.addEdge("A","B");
-        graph.addEdge("A","C");
-        graph.addEdge("A","D");
-        graph.addEdge("C","D");
-        graph.addEdge("A","E");
-       // graph.traverseDepthFirst("A");
-        graph.traverseBreadthFirst("A");
+        graph.addNode("P");
+        graph.addEdge("X","A");
+        graph.addEdge("X","B");
+        graph.addEdge("A","P");
+        graph.addEdge("B","P");
+        System.out.println(graph.topoligicalSort());
     }
+
     private class Node {
         private String label;
 
@@ -25,11 +35,15 @@ public class Graph {
             this.label = _label;
         }
 
+        @Override
+        public String toString() {
+            return  "node "+label;
+        }
     }
 
     private Map<String, Node> nodes = new HashMap<>();
     private Map<Node, List<Node>> adjacencyList = new HashMap<>();
-private Stack<Node> nodeStack= new Stack<>();
+    private Stack<Node> nodeStack = new Stack<>();
 
     public void addNode(String label) {
         var node = new Node(label);
@@ -59,14 +73,14 @@ private Stack<Node> nodeStack= new Stack<>();
     private void traverseDepthFirst(Node root, Set<Node> visited) {
         System.out.println(root.label);
         visited.add(root);
-        for(var node: adjacencyList.get(root)){
-            if(!visited.contains(node)){
-               traverseDepthFirst(node,visited);
+        for (var node : adjacencyList.get(root)) {
+            if (!visited.contains(node)) {
+                traverseDepthFirst(node, visited);
             }
         }
     }
 
-//    private void traverseDepthFirstByIteration(Node root){
+    //    private void traverseDepthFirstByIteration(Node root){
 //        System.out.println(root.label);
 //        nodeStack.add(root);
 //
@@ -75,24 +89,51 @@ private Stack<Node> nodeStack= new Stack<>();
 //            nodeStack.add(node.get(0));
 //        }
 //    }
-    public void traverseBreadthFirst(String root){
+    public void traverseBreadthFirst(String root) {
         traverseBreadthFirst(nodes.get(root), new HashSet<>());
     }
-    public void traverseBreadthFirst(Node root,Set<Node> visited){
-        var queue= new ArrayDeque<Node>();
+
+    public void traverseBreadthFirst(Node root, Set<Node> visited) {
+        var queue = new ArrayDeque<Node>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            var current= queue.remove();
-            if(visited.contains(current)){
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (visited.contains(current)) {
                 continue;
             }
             System.out.println(current.label);
-            for(var neighbor: adjacencyList.get(current)){
-                if(!visited.contains(neighbor)) {
+            for (var neighbor : adjacencyList.get(current)) {
+                if (!visited.contains(neighbor)) {
                     queue.add(neighbor);
                 }
             }
             visited.add(current);
         }
+    }
+
+    public List<Node> topoligicalSort() {
+        var visited = new HashSet<Node>();
+        var stack = new Stack<Node>();
+        for (var node : nodes.values()) {
+            topologicalSort(node, visited, stack);
+        }
+        List<Node> sorted = new ArrayList<>();
+
+        while (!stack.isEmpty()) {
+            sorted.add(stack.pop());
+        }
+        return sorted;
+    }
+
+    private void topologicalSort(Node node, Set<Node> visited, Stack<Node> stack) {
+        if (visited.contains(node)) {
+            return;
+        }
+        visited.add(node);
+
+        for (var neighbor : adjacencyList.get(node)) {
+            topologicalSort(neighbor, visited, stack);
+        }
+        stack.add(node);
     }
 }
