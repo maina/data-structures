7fd23452ece5770cc7cbe421c3063eb1a00265cc
James Bugara <james.bugara@mastercard.com>
2022-03-22 09:01:27 +0300

    Selection sort
    Signed-off-by: James Bugara <james.bugara@mastercard.com>

--- src/com/test/BubbleSort.java
+++ src/com/test/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         BubbleSort bubbleSort = new BubbleSort();
-        int[] unsortedArray = {3, 2, 1, 5, 4};
+        int[] unsortedArray = {7,1,3,4,6,2,3};
         var array = bubbleSort.sort(unsortedArray);
         System.out.println(Arrays.toString(array));
     }
@@ -13,7 +13,7 @@ public class BubbleSort {
     private int[] sort(int[] unSorted) {
         for (int i = 0; i < unSorted.length; i++) {
             for (var j = 1; j < unSorted.length; j++) {
-                if (unSorted[j] > unSorted[j - 1]) {
+                if (unSorted[j] < unSorted[j - 1]) {
                     var value = unSorted[j];
                     unSorted[j] = unSorted[j - 1];
                     unSorted[j - 1] = value;
